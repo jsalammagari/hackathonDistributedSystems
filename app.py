0ce247736a86f74529377dd581e5ee1a78cad7bf
Krushika Gujarati <krushika2910@gmail.com>
2023-11-18 14:38:39 -0800

    fetch response

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import os
 import sqlite3
 import PyPDF2
 import io
+import openai
 import os
 from PyPDF2 import PdfReader
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
 OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci/completions'
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+openai.api_key = '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'
 
 # SQLite Database setup
 DATABASE = 'chatbot.db'
@@ -19,13 +20,7 @@ DATABASE = 'chatbot.db'
 def init_db():
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
-    c.execute('''
-        CREATE TABLE IF NOT EXISTS chat_history (
-            id INTEGER PRIMARY KEY,
-            query TEXT,
-            response TEXT
-        )
-    ''')
+    c.execute('CREATE TABLE IF NOT EXISTS chat_history (id INTEGER PRIMARY KEY,query TEXT,response TEXT)')
     conn.commit()
     conn.close()
 
@@ -41,10 +36,12 @@ def store_query_response(query, response):
     conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
     c.execute('INSERT INTO chat_history (query, response) VALUES (?, ?)', (query, response))
+    listres = c.execute('SELECT * FROM chat_history')
+    print("data pront == >",listres)
     conn.commit()
     conn.close()
 
-PDF_FOLDER = '/pdfFolder'  # TODO: Update this path to the folder containing PDF files
+PDF_FOLDER = 'pdfFolder'  # TODO: Update this path to the folder containing PDF files
 
 def search_pdf_docs(query):
     for filename in os.listdir(PDF_FOLDER):
@@ -57,6 +54,7 @@ def search_pdf_docs(query):
                     if text and query.lower() in text.lower():
                         start = max(text.lower().find(query.lower()) - 30, 0)
                         end = min(start + 30 + len(query), len(text))
+                        print("text===>",text[start:end])
                         return text[start:end]  # Return the text surrounding the query
             except Exception as e:
                 print(f'Error reading {pdf_path}: {e}')
@@ -65,27 +63,41 @@ def search_pdf_docs(query):
 
 
 def get_response_from_openai(query):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {OPENAI_API_KEY}'
-    }
-    data = {
-        'prompt': query,
-        'max_tokens': 150
-    }
-    response = requests.post(OPENAI_API_URL, headers=headers, json=data)
-    if response.status_code == 200:
-        return response.json()['choices'][0]['text'].strip()
-    else:
-        return None
+    try:
+        response = openai.Completion.create(
+        model="text-davinci-002", 
+        prompt=query, 
+        max_tokens=150
+        )
+        answer = response.choices[0].text.strip()
+        return answer
+    except openai.error.OpenAIError as e:
+        # This will catch any OpenAI-specific errors
+        return  str(e)
+    except Exception as e:
+        return str(e)
+    # headers = {
+    #     'Content-Type': 'application/json',
+    #     'Authorization': f'Bearer {OPENAI_API_KEY}'
+    # }
+    # data = {
+    #     'prompt': query,
+    #     'max_tokens': 150
+    # }
+    # response = requests.post(OPENAI_API_URL, headers=headers, json=data)
+    # if response.status_code == 200:
+    #     return response.json()['choices'][0]['text'].strip()
+    # else:
+    #     return None
 
 @app.route('/')
 def index():
+    init_db()
     return render_template('index.html')
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    question = request.json.get('question')
+    question = request.form.get('question')
     
     # First, check in the database
     response = get_response_from_db(question)
@@ -97,11 +109,10 @@ def ask():
         if not response:
             # Finally, query OpenAI
             response = get_response_from_openai(question)
-
             # Store the new query and response in the database
             if response:
                 store_query_response(question, response)
-
+    print("last res===>>",response)
     return jsonify({'response': response})
 
 if __name__ == '__main__':
